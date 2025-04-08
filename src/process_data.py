cb6f1abc276d697982f31c87c809789df2c13f93
siljsols <siljsols@stud.ntnu.no>
2025-04-08 14:43:23 +0200

    Update process_data.py

--- src/process_data.py
+++ src/process_data.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
+# Renser og prosesserer værdata:
+# - Konverterer tid til datetime-format
+# - Gjetter riktige datatyper
+# - Fyller inn manglende verdier (lineær interpolering)
+# - Endrer kolonnenavn for temperatur 
 def process_and_clean_data(weather_data):
     df = pd.DataFrame(weather_data)
     df["time"] = pd.to_datetime(df["time"])
