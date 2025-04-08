d803875d6f72ca3b1688513662e06473f464d651
siljsols <siljsols@stud.ntnu.no>
2025-04-08 10:40:09 +0200

    Update test_hent_data.py

--- tests/test_hent_data.py
+++ tests/test_hent_data.py
@@ -7,7 +7,7 @@ from src.hent_dataene import hent_data_fra_frost
 
 # Bruk dummy data (kortere periode og kun én stasjon for test)
 def test_hent_data_med_gyldig_input():
-    client_id = "DUMMY_API_KEY"  # Bytt til gyldig nøkkel når du tester på ekte
+    client_id = "e0cdd794-6446-4380-9df0-e6828509519c"  #API-nøkkel
     stasjoner = {"Molde": "SN62295"}
     start = datetime(2023, 1, 1)
     slutt = datetime(2023, 1, 31)
@@ -28,3 +28,4 @@ def test_hent_data_med_ugyldig_stasjon():
     data = hent_data_fra_frost(client_id, stasjoner, start, slutt)
     assert isinstance(data, list)
     assert len(data) == 0  # Forventer tom liste
+
