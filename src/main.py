a1506f17ce865ddf439690ccba625c428136f582
Theadn03 <theadn@stud.ntnu.no>
2025-04-08 11:03:39 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from src.fetch_data import fetch_data_from_frost
 from src.process_data import process_and_clean_data
 from src.visualize_data import plot_temperature_trend
 
-# Oppsett
+# Setup
 client_id = "e0cdd794-6446-4380-9df0-e6828509519c"
 stations = {
     "Molde": "SN62295",
@@ -13,15 +13,15 @@ stations = {
 start_date = datetime(2020, 1, 1)
 end_date = datetime.now()
 
-# Kjøring
-print("Henter data...")
-weather_data = hent_data_fra_frost(client_id, stations, start_date, end_date)
+# Run
+print("Fetching data...")
+weather_data = fetch_data_from_frost(client_id, stations, start_date, end_date)
 
-print("Renser og bearbeider data...")
-df = behandle_og_rens_data(weather_data)
+print("Processing and cleaning data...")
+df = process_and_clean_data(weather_data)
 
-print("Lager visualisering...")
-vis_temperaturgraf(df)
+print("Creating visualization...")
+plot_temperature_trend(df)
 
 df.to_csv("ukedata.csv", index=False, encoding='utf-8')
-print("Ukedata lagret som 'ukedata.csv'")
+print("'ukedata.csv' saved.")
