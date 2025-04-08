7cbad6c8d31a35bd942ea9f819702e7b81a0b788
Theadn03 <theadn@stud.ntnu.no>
2025-04-08 11:08:14 +0200

    Update visualize_data.py

--- src/visualize_data.py
+++ src/visualize_data.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 
-def vis_temperaturgraf(df):
+def plot_temperature_trend(df):
     plt.figure(figsize=(12, 6))
     for city in df["Lokasjon"].unique():
         subset = df[df["Lokasjon"] == city]
         plt.plot(subset["Tidspunkt"], subset["Temperatur_C"], label=city)
-    plt.xlabel("Tid")
-    plt.ylabel("Temperatur (°C)")
-    plt.title("Temperatur kl 12:00 hver mandag fra 01.01.2020")
+    plt.xlabel("Time")
+    plt.ylabel("Temperature (°C)")
+    plt.title("Temperature at 12:00 every Monday from 01.01.2020")
     plt.legend()
     plt.grid()
     plt.show()
+
