c51b0a468f23a380c223f76d0619b5e68f4a41af
katrinerov <katrisro@stud.ntnu.no>
2025-05-21 17:40:57 +0200

    Update test_visualize_data to PEP8

--- tests/test_visualize_data.py
+++ tests/test_visualize_data.py
@@ -1,15 +1,19 @@
 # Tester at visualiseringen kjører uten feil
-from src.visualize_data import plot_temperature_trend
 import pandas as pd
+from src.visualize_data import plot_temperature_trend
 
-# Positiv test: gir korrekt formatert dataframe
+
 def test_plot_temperature_trend_runs():
-    data = pd.DataFrame ({
+    """
+    Tester at plot_temperature_trend kjører uten å kaste exception.
+    """
+    data = pd.DataFrame({
         "Location": ["Molde"],
         "Time": pd.date_range(start="2023-01-01", periods=1),
         "elementId": ["mean(air_temperature P1D)"],
         "value": [5]
     })
+
     try:
         plot_temperature_trend(data)
     except Exception as e:
